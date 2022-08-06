caa6b2110064118a5f1f0e3ea879f8f5972fa0f2
energylion <pozzonifabio-new@yahoo.it>
2022-08-06 20:35:01 +0200

    Initial commit

---
+++ src/color-button/color-button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-button',
+  templateUrl: './color-button.component.html',
+  styleUrls: ['./color-button.component.css']
+})
+export class ColorButtonComponent implements OnInit {
+
+  @Input()
+  input1 :string;
+
+  constructor() { }
+
+  ngOnInit() {
+    alert(this.input1);
+  }
+ 
+  Changecolor() : string {
+    
+  }
+
+}
